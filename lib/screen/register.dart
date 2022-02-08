689c3b813819b54fd7a293e76085ca87c53ceb7f
mgstabrani <13519122@std.stei.itb.ac.id>
2022-02-08 20:15:58 +0700

    move register to screen folder

--- lib/screen/register.dart
+++ lib/screen/register.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({Key? key}) : super(key: key);
+  static final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +12,52 @@ class RegisterPage extends StatelessWidget {
         backgroundColor: const Color.fromARGB(255, 196, 196, 196),
       ),
       body: Center(
-        child: Text('Register page'),
+        child: Form(
+          key: _formKey,
+          child: Container(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                TextFormField(
+                    decoration: InputDecoration(
+                  labelText: "Your Email (Child)",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0)),
+                )),
+                TextFormField(
+                    decoration: InputDecoration(
+                  labelText: "Password",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0)),
+                )),
+                TextFormField(
+                    decoration: InputDecoration(
+                  labelText: "Confirm Password",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0)),
+                )),
+                TextFormField(
+                    decoration: InputDecoration(
+                  labelText: "Parent Email",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0)),
+                )),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: const Color.fromARGB(255, 196, 196, 196),
+                  ),
+                  child: const Text(
+                    "Create Account",
+                    style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+                  ),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {}
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
