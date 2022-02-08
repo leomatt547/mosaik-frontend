992a884967a9e3745ffb3c389b4a26001eecc036
unknown <pratamandiko@gmail.com>
2022-02-08 18:57:08 +0700

    add button login and register to redirect

---
+++ lib/screen/login.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+        backgroundColor: const Color.fromARGB(255, 196, 196, 196),
+      ),
+      body: Center(
+        child: Text('Login page'),
+      ),
+    );
+  }
+}
---
+++ lib/screen/register.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatelessWidget {
+  const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register'),
+        backgroundColor: const Color.fromARGB(255, 196, 196, 196),
+      ),
+      body: Center(
+        child: Text('Register page'),
+      ),
+    );
+  }
+}
