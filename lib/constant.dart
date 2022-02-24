776e99799675d59de2a2d04fd67e61207975dde3
mgstabrani <13519122@std.stei.itb.ac.id>
2022-02-24 23:07:07 +0700

    refactor

--- lib/constant.dart
+++ lib/constant.dart
@@ -2,7 +2,9 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_storage/get_storage.dart';
 
 // ignore: non_constant_identifier_names
-String API_URL = dotenv.get('API_URL');
+String API_URL = 'https://mosaik-id.herokuapp.com';
+// Cannot implement async in android
+// String API_URL = dotenv.get('API_URL');
 
 // Token storage
 final storage = GetStorage();
