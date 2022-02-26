45ce5a51ea1c98c3d646ac8ee92280eeb67db570
Andres Jerriel Sinabutar <andres.sinabutar@gmail.com>
2022-02-26 14:54:23 +0700

    refactor registration page files

--- lib/screen/child_registration.dart
+++ lib/screen/child_registration.dart
@@ -2,13 +2,14 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:mosaic/screen/login.dart';
+import 'package:mosaic/screen/parent_registration.dart';
 import 'package:mosaic/widgets/appbar.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
 import 'package:http/http.dart' as http;
 import '../constant.dart';
 
-class RegisterChildPage extends StatelessWidget {
-  RegisterChildPage({Key? key}) : super(key: key);
+class ChildRegistration extends StatelessWidget {
+  ChildRegistration({Key? key}) : super(key: key);
   static final _formKey = GlobalKey<FormState>();
 
   TextEditingController nameController = TextEditingController();
@@ -133,184 +134,7 @@ class RegisterChildPage extends StatelessWidget {
                     ),
                     onPressed: () {
                       Route route = MaterialPageRoute(
-                          builder: (context) => RegisterParentPage());
-                      Navigator.push(context, route);
-                    },
-                  ),
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: const Color.fromARGB(255, 196, 196, 196),
-                  ),
-                  child: const Text(
-                    "Create Account",
-                    style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
-                  ),
-                  onPressed: () async {
-                    if (_formKey.currentState!.validate()) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text('Processing Data')),
-                      );
-                    }
-                    Map data = {
-                      'nama': nameController.text.toString(),
-                      'email': emailController.text.toString(),
-                      'password': passwordController.text.toString(),
-                    };
-
-                    String body = json.encode(data);
-                    final response = await http.post(
-                      Uri.parse(API_URL + "/parents"),
-                      body: body,
-                      encoding: Encoding.getByName('utf-8'),
-                    );
-
-                    if (response.statusCode == 201) {
-                      Alert(
-                        context: context,
-                        type: AlertType.success,
-                        title: "Registrasi berhasil",
-                        desc: "Selamat anda berhasil registrasi",
-                        buttons: [
-                          DialogButton(
-                            child: const Text(
-                              "Oke",
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 14),
-                            ),
-                            onPressed: () {
-                              Route route = MaterialPageRoute(
-                                  builder: (context) => LoginPage());
-                              Navigator.push(context, route);
-                            },
-                          )
-                        ],
-                      ).show();
-                      return;
-                    }
-                  },
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class RegisterParentPage extends StatelessWidget {
-  RegisterParentPage({Key? key}) : super(key: key);
-  static final _formKey = GlobalKey<FormState>();
-
-  TextEditingController nameController = TextEditingController();
-  TextEditingController emailController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-  TextEditingController confirmPasswordController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const CustomAppBar(),
-      resizeToAvoidBottomInset: false,
-      body: Center(
-        child: Form(
-          key: _formKey,
-          child: Container(
-            padding: const EdgeInsets.only(bottom: 20),
-            child: Column(
-              children: [
-                const Padding(
-                  padding: EdgeInsets.all(20),
-                  child: Text(
-                    'Welcome',
-                    style: TextStyle(fontSize: 28),
-                  ),
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.only(bottom: 20, left: 20, right: 20),
-                  child: TextFormField(
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Enter name';
-                        }
-                        return null;
-                      },
-                      controller: nameController,
-                      decoration: InputDecoration(
-                        labelText: "Name",
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(5.0)),
-                      )),
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.only(bottom: 20, left: 20, right: 20),
-                  child: TextFormField(
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Enter email';
-                        }
-                        return null;
-                      },
-                      controller: emailController,
-                      decoration: InputDecoration(
-                        labelText: "Your Email (Parent)",
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(5.0)),
-                      )),
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.only(bottom: 20, left: 20, right: 20),
-                  child: TextFormField(
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Enter password';
-                        }
-                        return null;
-                      },
-                      controller: passwordController,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        labelText: "Password",
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(5.0)),
-                      )),
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.only(bottom: 20, left: 20, right: 20),
-                  child: TextFormField(
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Enter confirm password';
-                        }
-                        return null;
-                      },
-                      controller: confirmPasswordController,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        labelText: "Confirm Password",
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(5.0)),
-                      )),
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.only(bottom: 20, left: 20, right: 20),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: const Color.fromARGB(255, 196, 196, 196),
-                    ),
-                    child: const Text(
-                      "Child Account",
-                      style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
-                    ),
-                    onPressed: () {
-                      Route route = MaterialPageRoute(
-                          builder: (context) => RegisterChildPage());
+                          builder: (context) => ParentRegistration());
                       Navigator.push(context, route);
                     },
                   ),
