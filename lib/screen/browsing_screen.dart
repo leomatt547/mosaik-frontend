de01932e5d3b4f9f79e7c7aa5d45d77a7e222297
unknown <pratamandiko@gmail.com>
2022-03-03 19:05:48 +0700

    add bar browsing page

--- lib/screen/browsing_screen.dart
+++ lib/screen/browsing_screen.dart
@@ -4,6 +4,7 @@ import 'package:mosaic/screen/history_screen.dart';
 import 'package:mosaic/screen/landing_screen.dart';
 import 'package:mosaic/screen/update_profile.dart';
 import 'package:flutter/gestures.dart';
+import 'package:mosaic/widgets/appbar.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class BrowsingScreen extends StatefulWidget {
@@ -26,6 +27,41 @@ class _BrowsingScreenState extends State<BrowsingScreen> {
 
   @override
   Widget build(BuildContext context) {
+    void onSelectedMoreOptions(BuildContext context, int item) {
+      switch (item) {
+        case 0:
+          print('New tab');
+          break;
+        case 1:
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => const HistoryScreen()),
+          );
+          break;
+        case 2:
+          print('Downloads');
+          break;
+        case 3:
+          print('go to Settings screen');
+          break;
+      }
+    }
+
+    void onSelectedAccount(BuildContext context, int item) {
+      switch (item) {
+        case 0:
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => UpdateProfilePage()),
+          );
+          break;
+        case 1:
+          print('Create CHild Account');
+          break;
+        case 2:
+          print('Logout');
+          break;
+      }
+    }
+
     FocusScopeNode currentFocus = FocusScope.of(context);
     return GestureDetector(
       onTap: () {
@@ -42,6 +78,188 @@ class _BrowsingScreenState extends State<BrowsingScreen> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
+                Flexible(
+                  flex: 1,
+                  child: Container(
+                    color: Color.fromARGB(255, 196, 196, 196),
+                    child: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Flexible(
+                            flex: 1,
+                            child: Center(
+                              child: IconButton(
+                                  icon: Icon(
+                                    Icons.cottage_outlined,
+                                    color: Colors.black,
+                                  ),
+                                  onPressed: () {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) => LandingPage(),
+                                        ));
+                                  }),
+                            ),
+                          ),
+                          Flexible(
+                            flex: 4,
+                            child: TextFormField(
+                              autocorrect: false,
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 18),
+                              decoration: InputDecoration(
+                                  prefixIcon: Icon(
+                                    Icons.lock,
+                                    color: Colors.white,
+                                  ),
+                                  isDense: true,
+                                  contentPadding: EdgeInsets.symmetric(
+                                      horizontal: 0, vertical: 7),
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(15))),
+                                  fillColor: Color.fromARGB(255, 103, 103, 103),
+                                  filled: true),
+                              onFieldSubmitted: (term) {
+                                String finalURL = _teController.text;
+                                if (!finalURL.startsWith("https://")) {
+                                  finalURL = "https://" + finalURL;
+                                }
+                                if (_webViewController != null) {
+                                  updateLoading(true);
+                                  _webViewController
+                                      .loadUrl(finalURL)
+                                      .then((onValue) {})
+                                      .catchError((e) {
+                                    updateLoading(false);
+                                  });
+                                }
+                              },
+                              controller: _teController,
+                            ),
+                          ),
+                          PopupMenuButton<int>(
+                            icon: Icon(
+                              Icons.account_circle,
+                              color: Colors.black,
+                            ),
+                            color: Color.fromARGB(255, 196, 196, 196),
+                            onSelected: (item) =>
+                                onSelectedAccount(context, item),
+                            itemBuilder: (context) => [
+                              PopupMenuItem<int>(
+                                value: 0,
+                                child: Row(
+                                  children: [
+                                    Icon(Icons.manage_accounts_rounded),
+                                    const SizedBox(width: 8),
+                                    Text(
+                                      'Manage Account',
+                                      style: TextStyle(color: Colors.black),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              PopupMenuItem<int>(
+                                value: 1,
+                                child: Row(
+                                  children: [
+                                    Icon(Icons.person_add),
+                                    const SizedBox(width: 8),
+                                    Text(
+                                      'Create Child Account',
+                                      style: TextStyle(color: Colors.black),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              PopupMenuItem<int>(
+                                value: 2,
+                                child: Row(
+                                  children: [
+                                    Icon(Icons.logout),
+                                    const SizedBox(width: 8),
+                                    Text(
+                                      'Log out',
+                                      style: TextStyle(color: Colors.black),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          Theme(
+                            data: Theme.of(context).copyWith(
+                              iconTheme: IconThemeData(color: Colors.black),
+                            ),
+                            child: PopupMenuButton<int>(
+                              color: Color.fromARGB(255, 196, 196, 196),
+                              onSelected: (item) =>
+                                  onSelectedMoreOptions(context, item),
+                              itemBuilder: (context) => [
+                                PopupMenuItem<int>(
+                                  value: 0,
+                                  child: Row(
+                                    children: [
+                                      Icon(Icons.add_box_outlined),
+                                      const SizedBox(width: 8),
+                                      Text(
+                                        'New tab',
+                                        style: TextStyle(color: Colors.black),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                PopupMenuItem<int>(
+                                  value: 1,
+                                  child: Row(
+                                    children: [
+                                      Icon(Icons.history),
+                                      const SizedBox(width: 8),
+                                      Text(
+                                        'History',
+                                        style: TextStyle(color: Colors.black),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                PopupMenuItem<int>(
+                                  value: 1,
+                                  child: Row(
+                                    children: [
+                                      Icon(Icons.download),
+                                      const SizedBox(width: 8),
+                                      Text(
+                                        'Download',
+                                        style: TextStyle(color: Colors.black),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                PopupMenuItem<int>(
+                                  value: 2,
+                                  child: Row(
+                                    children: [
+                                      Icon(Icons.settings),
+                                      const SizedBox(width: 8),
+                                      Text(
+                                        'Settings',
+                                        style: TextStyle(color: Colors.black),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
                 Flexible(
                     flex: 9,
                     child: Stack(
