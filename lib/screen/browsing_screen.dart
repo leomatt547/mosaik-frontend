bf8fd2a79a868ba03db2f208a7b48946ed485e2a
unknown <pratamandiko@gmail.com>
2022-03-16 12:19:31 +0700

    fixing browsing page

--- lib/screen/browsing_screen.dart
+++ lib/screen/browsing_screen.dart
@@ -9,6 +9,8 @@ import 'package:mosaic/screen/update_profile.dart';
 import 'package:flutter/gestures.dart';
 import 'package:mosaic/widgets/appbar.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class BrowsingScreen extends StatefulWidget {
   final String text;
@@ -71,101 +73,144 @@ class _BrowsingScreenState extends State<BrowsingScreen> {
     }
 
     FocusScopeNode currentFocus = FocusScope.of(context);
-    return GestureDetector(
-      onTap: () {
-        if (!currentFocus.hasPrimaryFocus) {
-          currentFocus.unfocus();
-        }
-      },
-      child: Scaffold(
-        resizeToAvoidBottomInset: false,
-        body: SafeArea(
-          child: Container(
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Flexible(
-                  flex: 1,
-                  child: Container(
-                    color: Color.fromARGB(255, 196, 196, 196),
-                    child: Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Flexible(
-                            flex: 1,
-                            child: Center(
-                              child: IconButton(
-                                  icon: Icon(
-                                    Icons.cottage_outlined,
-                                    color: Colors.black,
-                                  ),
-                                  onPressed: () {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) => LandingPage(),
-                                        ));
-                                  }),
-                            ),
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Flexible(
+                flex: 1,
+                child: Container(
+                  color: Color.fromARGB(255, 196, 196, 196),
+                  child: Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Flexible(
+                          flex: 1,
+                          child: Center(
+                            child: IconButton(
+                                icon: Icon(
+                                  Icons.cottage_outlined,
+                                  color: Colors.black,
+                                ),
+                                onPressed: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => LandingPage(),
+                                      ));
+                                }),
                           ),
-                          Flexible(
-                            flex: 4,
-                            child: TextFormField(
-                              autocorrect: false,
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 18),
-                              decoration: InputDecoration(
-                                  prefixIcon: Icon(
-                                    Icons.lock,
-                                    color: Colors.white,
-                                  ),
-                                  isDense: true,
-                                  contentPadding: EdgeInsets.symmetric(
-                                      horizontal: 0, vertical: 7),
-                                  border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.all(
-                                          Radius.circular(15))),
-                                  fillColor: Color.fromARGB(255, 103, 103, 103),
-                                  filled: true),
-                              onFieldSubmitted: (term) {
-                                String finalURL = _teController.text;
-                                if (!finalURL.startsWith("https://")) {
-                                  finalURL = "https://" + finalURL;
-                                }
-                                if (_webViewController != null) {
-                                  updateLoading(true);
-                                  _webViewController
-                                      .loadUrl(finalURL)
-                                      .then((onValue) {})
-                                      .catchError((e) {
-                                    updateLoading(false);
-                                  });
-                                }
-                              },
-                              controller: _teController,
-                            ),
+                        ),
+                        Flexible(
+                          flex: 4,
+                          child: TextFormField(
+                            autocorrect: false,
+                            style: TextStyle(color: Colors.white, fontSize: 18),
+                            decoration: InputDecoration(
+                                prefixIcon: Icon(
+                                  Icons.lock,
+                                  color: Colors.white,
+                                ),
+                                isDense: true,
+                                contentPadding: EdgeInsets.symmetric(
+                                    horizontal: 0, vertical: 7),
+                                border: OutlineInputBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(15))),
+                                fillColor: Color.fromARGB(255, 103, 103, 103),
+                                filled: true),
+                            onFieldSubmitted: (term) {
+                              String finalURL = _teController.text;
+                              if (!finalURL.startsWith("https://")) {
+                                finalURL = "https://" + finalURL;
+                              }
+                              if (_webViewController != null) {
+                                updateLoading(true);
+                                _webViewController
+                                    .loadUrl(finalURL)
+                                    .then((onValue) {})
+                                    .catchError((e) {
+                                  updateLoading(false);
+                                });
+                              }
+                            },
+                            controller: _teController,
                           ),
-                          PopupMenuButton<int>(
-                            icon: Icon(
-                              Icons.account_circle,
-                              color: Colors.black,
+                        ),
+                        PopupMenuButton<int>(
+                          icon: Icon(
+                            Icons.account_circle,
+                            color: Colors.black,
+                          ),
+                          color: Color.fromARGB(255, 196, 196, 196),
+                          onSelected: (item) =>
+                              onSelectedAccount(context, item),
+                          itemBuilder: (context) => [
+                            PopupMenuItem<int>(
+                              value: 0,
+                              child: Row(
+                                children: [
+                                  Icon(Icons.manage_accounts_rounded),
+                                  const SizedBox(width: 8),
+                                  Text(
+                                    'Manage Account',
+                                    style: TextStyle(color: Colors.black),
+                                  ),
+                                ],
+                              ),
                             ),
+                            PopupMenuItem<int>(
+                              value: 1,
+                              child: Row(
+                                children: [
+                                  Icon(Icons.person_add),
+                                  const SizedBox(width: 8),
+                                  Text(
+                                    'Create Child Account',
+                                    style: TextStyle(color: Colors.black),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            PopupMenuItem<int>(
+                              value: 2,
+                              child: Row(
+                                children: [
+                                  Icon(Icons.logout),
+                                  const SizedBox(width: 8),
+                                  Text(
+                                    'Log out',
+                                    style: TextStyle(color: Colors.black),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        Theme(
+                          data: Theme.of(context).copyWith(
+                            iconTheme: IconThemeData(color: Colors.black),
+                          ),
+                          child: PopupMenuButton<int>(
                             color: Color.fromARGB(255, 196, 196, 196),
                             onSelected: (item) =>
-                                onSelectedAccount(context, item),
+                                onSelectedMoreOptions(context, item),
                             itemBuilder: (context) => [
                               PopupMenuItem<int>(
-                                value: 0,
+                                value: 1,
                                 child: Row(
                                   children: [
-                                    Icon(Icons.manage_accounts_rounded),
+                                    Icon(Icons.history),
                                     const SizedBox(width: 8),
                                     Text(
-                                      'Manage Account',
+                                      'History',
                                       style: TextStyle(color: Colors.black),
                                     ),
                                   ],
@@ -175,10 +220,10 @@ class _BrowsingScreenState extends State<BrowsingScreen> {
                                 value: 1,
                                 child: Row(
                                   children: [
-                                    Icon(Icons.person_add),
+                                    Icon(Icons.download),
                                     const SizedBox(width: 8),
                                     Text(
-                                      'Create Child Account',
+                                      'Download',
                                       style: TextStyle(color: Colors.black),
                                     ),
                                   ],
@@ -188,10 +233,10 @@ class _BrowsingScreenState extends State<BrowsingScreen> {
                                 value: 2,
                                 child: Row(
                                   children: [
-                                    Icon(Icons.logout),
+                                    Icon(Icons.settings),
                                     const SizedBox(width: 8),
                                     Text(
-                                      'Log out',
+                                      'Settings',
                                       style: TextStyle(color: Colors.black),
                                     ),
                                   ],
@@ -199,93 +244,64 @@ class _BrowsingScreenState extends State<BrowsingScreen> {
                               ),
                             ],
                           ),
-                          Theme(
-                            data: Theme.of(context).copyWith(
-                              iconTheme: IconThemeData(color: Colors.black),
-                            ),
-                            child: PopupMenuButton<int>(
-                              color: Color.fromARGB(255, 196, 196, 196),
-                              onSelected: (item) =>
-                                  onSelectedMoreOptions(context, item),
-                              itemBuilder: (context) => [
-                                PopupMenuItem<int>(
-                                  value: 1,
-                                  child: Row(
-                                    children: [
-                                      Icon(Icons.history),
-                                      const SizedBox(width: 8),
-                                      Text(
-                                        'History',
-                                        style: TextStyle(color: Colors.black),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                PopupMenuItem<int>(
-                                  value: 1,
-                                  child: Row(
-                                    children: [
-                                      Icon(Icons.download),
-                                      const SizedBox(width: 8),
-                                      Text(
-                                        'Download',
-                                        style: TextStyle(color: Colors.black),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                PopupMenuItem<int>(
-                                  value: 2,
-                                  child: Row(
-                                    children: [
-                                      Icon(Icons.settings),
-                                      const SizedBox(width: 8),
-                                      Text(
-                                        'Settings',
-                                        style: TextStyle(color: Colors.black),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
-                Flexible(
-                    flex: 9,
-                    child: Stack(
-                      children: <Widget>[
-                        WebView(
-                          initialUrl: 'https://' + widget.text,
-                          onPageFinished: (data) {
-                            print(_webViewController.currentUrl());
-                            updateLoading(false);
-                            _webViewController.currentUrl().then((value) =>
-                                _teController.text = (value.toString()));
-                          },
-                          javascriptMode: JavascriptMode.unrestricted,
-                          onWebViewCreated: (webViewController) {
-                            _webViewController = webViewController;
-                          },
-                        ),
-                        GestureDetector(
-                          behavior: HitTestBehavior.translucent,
-                          onTapDown: (_) => currentFocus.unfocus(),
-                          child: Container(),
-                        ),
-                        (showLoading)
-                            ? Center(
-                                child: CircularProgressIndicator(),
-                              )
-                            : Center()
-                      ],
-                    )),
-              ],
-            ),
+              ),
+              Flexible(
+                  flex: 9,
+                  child: Stack(
+                    children: <Widget>[
+                      WebView(
+                        initialUrl: widget.text.startsWith("https://")
+                            ? widget.text
+                            : "https://" + widget.text,
+                        onPageFinished: (data) async {
+                          updateLoading(false);
+                          _webViewController.currentUrl().then((value) async {
+                            _teController.text = (value.toString());
+                            Map data = {
+                              "url": value.toString(),
+                              "title": value.toString()
+                            };
+
+                            String bodyNewUrl = json.encode(data);
+                            final newUrl = await http.post(
+                              Uri.parse(API_URL + "/urls"),
+                              body: bodyNewUrl,
+                              encoding: Encoding.getByName('utf-8'),
+                            );
+                            Map dataVisit = {
+                              "url_id": json.decode(newUrl.body)["id"],
+                              "duration": 3,
+                              "parent_id": storage.read('parent_id')
+                            };
+                            String bodyNewVisit = json.encode(dataVisit);
+                            final newVisit = await http.post(
+                                Uri.parse(API_URL + "/parentvisits"),
+                                body: bodyNewVisit,
+                                encoding: Encoding.getByName('utf-8'),
+                                headers: {
+                                  'Authorization':
+                                      'Bearer ' + storage.read('token')
+                                });
+                          });
+                        },
+                        javascriptMode: JavascriptMode.unrestricted,
+                        onWebViewCreated: (webViewController) {
+                          _webViewController = webViewController;
+                        },
+                      ),
+                      (showLoading)
+                          ? Center(
+                              child: CircularProgressIndicator(),
+                            )
+                          : Center()
+                    ],
+                  )),
+            ],
           ),
         ),
       ),
