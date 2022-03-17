05816c3d19049210d546cc7e770a120d2b98610b
Andres Jerriel Sinabutar <andres.sinabutar@gmail.com>
2022-03-17 10:26:15 +0700

    make parent registration screen become a stateful widget

--- lib/screen/parent_registration_screen.dart
+++ lib/screen/parent_registration_screen.dart
@@ -1,20 +1,67 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:mosaic/screen/login.dart';
 import 'package:mosaic/widgets/appbar.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
 import 'package:http/http.dart' as http;
 import '../constant.dart';
 
-class ParentRegistration extends StatelessWidget {
-  ParentRegistration({Key? key}) : super(key: key);
+
+class ParentRegistrationScreen extends StatefulWidget {
+  const ParentRegistrationScreen({Key? key}) : super(key: key);
+
+  @override
+  _ParentRegistrationScreenState createState() => _ParentRegistrationScreenState();
+}
+
+class _ParentRegistrationScreenState extends State<ParentRegistrationScreen> {
+
   static final _formKey = GlobalKey<FormState>();
 
   TextEditingController nameController = TextEditingController();
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   TextEditingController confirmPasswordController = TextEditingController();
+
+  var bodyProgress = Container(
+    decoration: BoxDecoration(
+        color: Colors.grey[200],
+        borderRadius: BorderRadius.circular(10.0)
+    ),
+    width: 300.0,
+    height: 200.0,
+    alignment: AlignmentDirectional.center,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        const Center(
+          child: SizedBox(
+            height: 50.0,
+            width: 50.0,
+            child: CircularProgressIndicator(
+              value: null,
+              strokeWidth: 7.0,
+              color: Colors.black,
+            ),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 25.0),
+          child: Center(
+            child: Text(
+                "Processing...",
+                style: GoogleFonts.average(
+                  fontWeight: FontWeight.w700,
+                )
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -28,16 +75,19 @@ class ParentRegistration extends StatelessWidget {
             padding: const EdgeInsets.only(bottom: 20),
             child: Column(
               children: [
-                const Padding(
+                Padding(
                   padding: EdgeInsets.all(20),
                   child: Text(
                     'Welcome',
-                    style: TextStyle(fontSize: 28),
+                    style: GoogleFonts.average(
+                      fontSize: 28,
+                      fontWeight: FontWeight.w700,
+                    ),
                   ),
                 ),
                 Padding(
                   padding:
-                      const EdgeInsets.only(bottom: 20, left: 20, right: 20),
+                  const EdgeInsets.only(bottom: 20, left: 20, right: 20),
                   child: TextFormField(
                       validator: (value) {
                         if (value == null || value.isEmpty) {
@@ -54,7 +104,7 @@ class ParentRegistration extends StatelessWidget {
                 ),
                 Padding(
                   padding:
-                      const EdgeInsets.only(bottom: 20, left: 20, right: 20),
+                  const EdgeInsets.only(bottom: 20, left: 20, right: 20),
                   child: TextFormField(
                       validator: (value) {
                         if (value == null || value.isEmpty) {
@@ -71,7 +121,7 @@ class ParentRegistration extends StatelessWidget {
                 ),
                 Padding(
                   padding:
-                      const EdgeInsets.only(bottom: 20, left: 20, right: 20),
+                  const EdgeInsets.only(bottom: 20, left: 20, right: 20),
                   child: TextFormField(
                       validator: (value) {
                         if (value == null || value.isEmpty) {
@@ -89,7 +139,7 @@ class ParentRegistration extends StatelessWidget {
                 ),
                 Padding(
                   padding:
-                      const EdgeInsets.only(bottom: 20, left: 20, right: 20),
+                  const EdgeInsets.only(bottom: 20, left: 20, right: 20),
                   child: TextFormField(
                       validator: (value) {
                         if (value == null || value.isEmpty) {
@@ -109,16 +159,14 @@ class ParentRegistration extends StatelessWidget {
                   style: ElevatedButton.styleFrom(
                     primary: const Color.fromARGB(255, 196, 196, 196),
                   ),
-                  child: const Text(
+                  child: Text(
                     "Create Account",
-                    style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+                    style: GoogleFonts.average(
+                      fontWeight: FontWeight.w700,
+                      color: Colors.black
+                    ),
                   ),
                   onPressed: () async {
-                    if (_formKey.currentState!.validate()) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text('Processing Data')),
-                      );
-                    }
                     Map data = {
                       'nama': nameController.text.toString(),
                       'email': emailController.text.toString(),
@@ -126,35 +174,27 @@ class ParentRegistration extends StatelessWidget {
                     };
 
                     String body = json.encode(data);
+
+                    showDialog(
+                      context: context,
+                      barrierDismissible: false,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          content: bodyProgress,
+                          contentPadding: EdgeInsets.zero,
+                          backgroundColor: Colors.transparent,
+                        );
+                      },
+                    );
+
                     final response = await http.post(
                       Uri.parse(API_URL + "/parents"),
                       body: body,
                       encoding: Encoding.getByName('utf-8'),
                     );
 
-                    if (response.statusCode == 201) {
-                      Alert(
-                        context: context,
-                        type: AlertType.success,
-                        title: "Success",
-                        desc: "Your account has been successfully created",
-                        buttons: [
-                          DialogButton(
-                            child: const Text(
-                              "OK",
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 14),
-                            ),
-                            onPressed: () {
-                              Route route = MaterialPageRoute(
-                                  builder: (context) => LoginPage());
-                              Navigator.push(context, route);
-                            },
-                          )
-                        ],
-                      ).show();
-                      return;
-                    }
+                    Navigator.pop(context); //pop dia
+                    _register(response);
                   },
                 ),
               ],
@@ -164,4 +204,31 @@ class ParentRegistration extends StatelessWidget {
       ),
     );
   }
+
+  void _register(response) {
+    if (response.statusCode == 201) {
+      Alert(
+        context: context,
+        type: AlertType.success,
+        title: "Success",
+        desc: "Your account has been successfully created",
+        buttons: [
+          DialogButton(
+            child: const Text(
+              "OK",
+              style:
+              TextStyle(color: Colors.white, fontSize: 14),
+            ),
+            onPressed: () {
+              Route route = MaterialPageRoute(
+                  builder: (context) => LoginPage());
+              Navigator.push(context, route);
+            },
+          )
+        ],
+      ).show();
+    }
+  }
+
 }
+
