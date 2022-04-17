9128d460fb190a843c088716473c6166c3a5aea6
Andres Jerriel Sinabutar <andres.sinabutar@gmail.com>
2022-04-17 17:50:53 +0700

    finish manage child screen

--- lib/screen/manage_child/manage_child_screen.dart
+++ lib/screen/manage_child/manage_child_screen.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:mosaic/models/childs.dart';
+import 'package:mosaic/screen/manage_child/edit_child_profile_screen.dart';
 import 'package:mosaic/utils/colors.dart';
 import 'package:mosaic/constant.dart';
 import 'package:http/http.dart' as http;
 import 'package:nb_utils/nb_utils.dart';
 import 'dart:convert';
-
 
 class ManageChildScreen extends StatefulWidget {
   const ManageChildScreen({Key? key}) : super(key: key);
@@ -15,7 +15,7 @@ class ManageChildScreen extends StatefulWidget {
   State<ManageChildScreen> createState() => _ManageChildScreenState();
 }
 
-class _ManageChildScreenState extends State<ManageChildScreen> {
+class _ManageChildScreenState extends State<ManageChildScreen>{
   List<Child> _userChildren = [];
   bool _isLoading = false;
 
@@ -29,6 +29,7 @@ class _ManageChildScreenState extends State<ManageChildScreen> {
     setState(() {
       _isLoading = true;
     });
+
     late Uri url;
 
     url = Uri.parse(
@@ -37,10 +38,12 @@ class _ManageChildScreenState extends State<ManageChildScreen> {
     try {
       final response = await http.get(url);
       List<dynamic> extractedData = json.decode(response.body);
+
       // ignore: unnecessary_null_comparison
       if (extractedData == null) {
         return;
       }
+
       List<Child> loadedProducts = [];
       loadedProducts = extractedData.map((dynamic childResponse) {
         String id = childResponse['id'].toString();
@@ -48,6 +51,7 @@ class _ManageChildScreenState extends State<ManageChildScreen> {
         String email = childResponse['email'];
         return Child(id: id, nama: nama, email: email);
       }).toList();
+
       setState(() {
         _isLoading = false;
         _userChildren = loadedProducts.reversed.toList();
@@ -55,30 +59,6 @@ class _ManageChildScreenState extends State<ManageChildScreen> {
     } catch (error) {
       rethrow;
     }
-  }
-
-  Future<void> deleteChild(String? id) async {
-
-    setState(() {
-      _isLoading = true;
-    });
-
-    late Uri url;
-    url = Uri.parse(API_URL + "/childs/" + id!);
-
-    final existingProductIndex =
-        _userChildren.indexWhere((hist) => hist.id == id);
-    var existingProduct = _userChildren[existingProductIndex];
-    _userChildren.removeAt(existingProductIndex);
-    final response = await http.delete(url,
-        headers: {'Authorization': 'Bearer ' + storage.read('token')});
-    if (response.statusCode >= 400) {
-      _userChildren.insert(existingProductIndex, existingProduct);
-    }
-    setState(() {
-      _isLoading = false;
-    });
-    // existingProduct = null;
   }
 
   @override
@@ -114,7 +94,8 @@ class _ManageChildScreenState extends State<ManageChildScreen> {
                 width: context.width(),
                 decoration: BoxDecoration(
                     image: DecorationImage(
-                        image: AssetImage('assets/bg1.jpg'), fit: BoxFit.cover)),
+                        image: AssetImage('assets/bg1.jpg'),
+                        fit: BoxFit.cover)),
                 child: _isLoading
                     ? const Center(
                         child: CircularProgressIndicator(
@@ -134,48 +115,44 @@ class _ManageChildScreenState extends State<ManageChildScreen> {
                                     vertical: 8,
                                     horizontal: 5,
                                   ),
-                                  child: ListTile(
-                                      leading: Icon(
-                                        Icons.person,
-                                        color: primaryColor,
-                                        size: 56,
-                                      ),
-                                      title: Text(
-                                        _userChildren[index].nama.toString(),
-                                        overflow: TextOverflow.ellipsis,
-                                        style: const TextStyle(
-                                          fontWeight: FontWeight.w700,
-                                        ),
-                                      ),
-                                      subtitle: Text(
-                                        _userChildren[index].email.toString(),
-                                        overflow: TextOverflow.ellipsis,
-                                        style: const TextStyle(
-                                          color: Colors.grey,
-                                          fontWeight: FontWeight.w500,
-                                        ),
-                                      ),
-                                      trailing: storage.read('parent_id') !=
-                                              null
-                                          ? IconButton(
-                                              icon: const Icon(Icons.delete),
-                                              color: Colors.black,
-                                              onPressed: () {
-                                                showConfirmDialogCustom(
-                                                  context,
-                                                  title: "Delete Account",
-                                                  subTitle:
-                                                      'Are you sure want to delete this account?',
-                                                  dialogType: DialogType.DELETE,
-                                                  onAccept: (buildContext) {
-                                                    deleteChild(
-                                                        _userChildren[index]
-                                                            .id
-                                                            .toString());
-                                                  },
-                                                );
-                                              })
-                                          : null));
+                                  child: InkWell(
+                                      onTap: () async {
+                                        final result = await EditChildProfileScreen(
+                                                id: _userChildren[index]
+                                                    .id
+                                                    .toString())
+                                            .launch(context);
+                                        if (result) {
+                                          setState(() {
+                                            fetchAndSetChild();
+                                          });
+                                        }
+                                      },
+                                      child: ListTile(
+                                          leading: Icon(
+                                            Icons.person,
+                                            color: primaryColor,
+                                            size: 56,
+                                          ),
+                                          title: Text(
+                                            _userChildren[index]
+                                                .nama
+                                                .toString(),
+                                            overflow: TextOverflow.ellipsis,
+                                            style: const TextStyle(
+                                              fontWeight: FontWeight.w700,
+                                            ),
+                                          ),
+                                          subtitle: Text(
+                                            _userChildren[index]
+                                                .email
+                                                .toString(),
+                                            overflow: TextOverflow.ellipsis,
+                                            style: const TextStyle(
+                                              color: Colors.grey,
+                                              fontWeight: FontWeight.w500,
+                                            ),
+                                          ))));
                             },
                             itemCount: _userChildren.length,
                           ))));
